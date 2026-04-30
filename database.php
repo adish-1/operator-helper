806e131cd70760a8c4960b521b7da22dcdcddd58
adish-1 <adishjagan48@gmail.com>
2026-04-30 11:32:33 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+ $host="sql100.infinityfree.com";
+ $dbuser="if0_41420726";
+ $dbpass="LTp2R8ZmOJCFda9";
+ $dbname="if0_41420726_survey_2026";
+ $conne=mysqli_connect($host,$dbuser,$dbpass,$dbname);
+ if(!$conne)
+ {
+  die("<h3>connection failed</h3>");
+ }
+?>
